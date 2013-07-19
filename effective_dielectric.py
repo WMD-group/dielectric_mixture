e5ec38100f730e7ec80afd04b3a1740db3d98bf1
Adam Jackson <a.j.jackson@bath.ac.uk>
2013-07-19 10:47:00 +0100

    Added options handling

--- effective_dielectric.py
+++ effective_dielectric.py
@@ -1,23 +1,55 @@
+#! /usr/bin/env python
+
+"""Calculate the effective dielectric constant of crystals packed in a 
+dielectric medium using effective medium theory (Bruggeman 1935)."""
+
+################################################################################
+# Ruth Lunt 2013                                                               #
+################################################################################
+
 from numpy import linspace
 import matplotlib.pyplot as plt
+from optparse import OptionParser
 
+######################## Set up optional arguments #############################
+parser = OptionParser()
+parser.add_option("-d", "--dummy-option",
+                  action="store", type="float", dest="dummy_variable", default=5.0,
+                  help="This is a dummy option [default: 5.0]")
+parser.add_option("-b", "--bulk-dialectric-constant",
+                  action="store", type="float", dest="e1", default=0.,
+                  help="Dielectric constant for bulk medium")
+parser.add_option("-c", "--crystal-dialectric-constant",
+                  action="store", type="float", dest="e2", default=0.,
+                  help="Dielectric constant for packed/dispersed crystals")
+### Further options go here ###
+(options,args) = parser.parse_args()
+
+########################### Begin main program #################################
 print "A program to calculate the effective dielectric constant of crystals packed in a dielectric medium."
 # Based on effective medium theory (Bruggeman 1935)
 
 print "Ruth Lunt 2013\nDate last edited: 17/07/2013"
 
-#dielectric constant of material A
-e1 = raw_input("What is the dielectric constant of the bulk crystal?")
-e1 = float(e1)
+# Get dielectric constant of material A if not already set
+if options.e1 ==0:
+    e1 = raw_input("What is the dielectric constant of the bulk crystal?")
+    e1 = float(e1)
+else:
+    e1 = options.e1
 
-#dielectric constant of material B
-e2 = raw_input("What is the dielectric constant of the medium? (Hint: H2O = 80)")
-e2 = float(e2)
+# Get dielectric constant of material B if not already set
+if options.e2 == 0:
+    e2 = raw_input("What is the dielectric constant of the medium? (Hint: H2O = 80)")
+    e2 = float(e2)
+else:
+    e2 = options.e2
 
 #dimension 
 d = 3
 
-void_fraction = 0.4764
+# I think this is redundant?
+#void_fraction = 0.4764
 
 def effective_dielectric(e1, e2, void_fraction, d):
     #volume fraction of bulk crystal
